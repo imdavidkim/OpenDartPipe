7050bc025d9c7418a889f4a23bf86784b4e1b69c
imdavidkim <35369385+imdavidkim@users.noreply.github.com>
2020-12-14 14:16:01 +0900

    Update opendart_disclosure_info.py

--- opendart_disclosure_info.py
+++ opendart_disclosure_info.py
@@ -15,48 +15,85 @@ except ImportError:
 
 def getConfig():
     import configparser
-    global doc_path
+    global path, doc_path, apikey
     config = configparser.ConfigParser()
     config.read('config.ini')
+    apikey = config['DART']['SEARCH-API-KEY']
+    path = config['COMMON']['REPORT_PATH']
     doc_path = config['COMMON']['DOCUMENT_PATH']
+
+
+def getKey():
+    return apikey
 
 
 def get_corpcode_dict(crtfc_key):
     print("[OPENDART]기업고유번호 수집...")
-    params = {'crtfc_key': crtfc_key}
-    url = "https://opendart.fss.or.kr/api/corpCode.xml"
-    res = requests.get(url, params=params)
-    zfile = zipfile.ZipFile(io.BytesIO(res.content))
-    fin = zfile.open(zfile.namelist()[0])
-    # print(fin.read().decode('utf-8'))
-    root = ET.fromstring(fin.read().decode('utf-8'))
-    data = {}
-    for child in root:
-        if len(child.find('corp_code').text.strip()) > 1: # 종목고유코드가 있는 경우
-            data[child.find('corp_code').text.strip()] = {"stock_code": child.find('stock_code').text.strip(), "corp_name": child.find('corp_name').text.strip()}
-    print("{} CorpCode returned".format(len(data)))
+    getConfig()
+    data = None
+    yyyymmdd = str(datetime.now())[:10]
+    workDir = r'{}\{}\{}'.format(path, "DartCorpCode", yyyymmdd)
+    if not os.path.exists(workDir):
+        os.makedirs(workDir)
+    if os.path.exists(r'{}\DartCorpCode.{}.json'.format(workDir, yyyymmdd)):
+        with open(r'{}\DartCorpCode.{}.json'.format(workDir, yyyymmdd), "r") as f:
+            print(r"Read from {}\DartCorpCode.{}.json".format(workDir, yyyymmdd))
+            corpcodes = f.read()
+            data = json.loads(corpcodes)
+    if data is None:
+        params = {'crtfc_key': crtfc_key}
+        url = "https://opendart.fss.or.kr/api/corpCode.xml"
+        res = requests.get(url, params=params)
+        zfile = zipfile.ZipFile(io.BytesIO(res.content))
+        fin = zfile.open(zfile.namelist()[0])
+        # print(fin.read().decode('utf-8'))
+        root = ET.fromstring(fin.read().decode('utf-8'))
+        data = {}
+        for child in root:
+            if len(child.find('corp_code').text.strip()) > 1: # 종목고유코드가 있는 경우
+                data[child.find('corp_code').text.strip()] = {"corp_code": child.find('corp_code').text.strip(), "stock_code": child.find('stock_code').text.strip(), "corp_name": child.find('corp_name').text.strip()}
+        print("{} CorpCode returned".format(len(data)))
+        with open(r'{}\DartCorpCode.{}.json'.format(workDir, yyyymmdd), 'w') as fp:
+            json.dump(data, fp)
     return data
 
 
-def get_list_json(crtfc_key, corp_code=None, bgn_de=None, end_de=None, last_reprt_at=None, pblntf_ty=None, pblntf_detail_ty=None, corp_cls=None, sort=None, sort_mth=None, page_no=None, page_count=None):
+def get_list_json(crtfc_key, corp_code=None, bgn_de=None, end_de=None, last_reprt_at="Y", pblntf_ty=None, pblntf_detail_ty=None, corp_cls=None, sort="date", sort_mth="desc", page_no=None, page_count=100):
     print("[OPENDART]공시List 수집...")
+    retJson = None
+    start = pd.to_datetime(bgn_de).strftime('%Y%m%d') if bgn_de else ''
+    end = pd.to_datetime(end_de).strftime('%Y%m%d') if end_de else datetime.today().strftime('%Y%m%d')
     params = {'crtfc_key': crtfc_key
               , 'corp_code': corp_code
-              , 'bgn_de': bgn_de
-              , 'end_de': end_de
+              , 'bgn_de': start
+              , 'end_de': end
               , 'last_reprt_at': last_reprt_at
               , 'pblntf_ty': pblntf_ty
               , 'pblntf_detail_ty': pblntf_detail_ty
               , 'corp_cls': corp_cls
               , 'sort': sort
               , 'sort_mth': sort_mth
-              , 'page_no': page_no
+              , 'page_no': 1 if page_no is None else page_no
               , 'page_count': page_count
     }
     url = "https://opendart.fss.or.kr/api/list.json"
     res = requests.get(url, params=params)
     list_data = json.loads(res.content)
-    return list_data
+    retJson = list_data
+    if list_data["total_page"] != list_data["page_no"]:
+        cur_page = list_data["page_no"] + 1
+        tot_page = list_data["total_page"]
+        retJson = list_data
+        for page in range(cur_page, tot_page+1):
+            params["page_no"] = page
+            res = requests.get(url, params=params)
+            list_data = json.loads(res.content)
+            retJson["list"].extend(list_data["list"])
+        # print(retJson["total_count"], len(retJson["list"]))
+    retJson.pop("page_no", None)
+    retJson.pop("page_count", None)
+    retJson.pop("total_page", None)
+    return retJson
 
 
 def get_company_info_json(crtfc_key, corp_code=None):
